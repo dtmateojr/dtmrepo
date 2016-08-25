7b9040bf0e871d3e46a6011c282bf47e61a06731
Demosthenes Mateo Jr <dmateo@t420.home.net>
2016-08-25 22:26:22 +1000

    WARNING WARNING WARNING
    After a few more dtmrepo deployments in our organisation, I have decided
    to make further enhancements which may cause some inconvenience if you
    have existing dtmrepo installations.
    
    dtmrepo now uses --norepopath option during reposync so it may
    redownload packages that have already been downloaded.
    
    dtmrepo now assumes that it has been installed under INSTALLDIR as
    specified in the main dtmrepo script. Make sure that this has been set
    properly or use the bundled installer.sh. Read instructions.txt for more
    detailed info.
    
    More changes:
        - installer.sh has been added and instructions.txt has been
          simplified to reflect this
        - http_proxy, https_proxy and ftp_proxy are automatically unset to
          avoid conflicts with repo specific proxy configurations

---
+++ installer.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+INSTALLDIR=$1
+REPODIR=$2
+SUBSCRIBE=$3
+
+if [ -z "$INSTALLDIR" ]; then
+    echo "Usage: $(basename $0) <installation dir> [repo dir]"
+    exit 1
+fi
+
+if [ -z "$REPODIR" ]; then
+    REPODIR="$INSTALLDIR/repos"
+fi
+
+mkdir -p $INSTALLDIR/{bin,etc/{dtmrepo.conf.d,dtmrepo.repos.d},var/{log,cache/dtmrepo}} $REPODIR
+if [ "$?" -ne 0 ]; then
+    echo "Error creating installation directory tree."
+    exit 1
+fi
+
+echo -e "#!/bin/bash\n\nINSTALLDIR=$INSTALLDIR" > $INSTALLDIR/bin/dtmrepo
+tail -n +4 dtmrepo >> $INSTALLDIR/bin/dtmrepo
+\cp readconf dtmrepo-repoconf $INSTALLDIR/bin && chmod +x $INSTALLDIR/bin/*
+
+cat <<EOF > $INSTALLDIR/etc/dtmrepo.conf
+[global]
+rootdir = $REPODIR
+confdir = $INSTALLDIR/etc/dtmrepo.conf.d
+yumconf = $INSTALLDIR/etc/dtmrepo.repos.d/dtmrepo-yum.conf
+keep = 3
+arch = x86_64
+subscribed = 0
+EOF
+
+cat <<EOF > $INSTALLDIR/etc/dtmrepo.repos.d/dtmrepo-yum.conf
+[main]
+cachedir = $INSTALLDIR/var/cache/dtmrepo
+logfile = $INSTALLDIR/var/log/dtmrepo.log
+obsoletes = 1
+plugins = 1
+reposdir = $INSTALLDIR/etc/dtmrepo.repos.d
+EOF
+
+yum -y install httpd
+\cp httpd-dtmrepo.conf /etc/httpd/conf.d/dtmrepo.conf
+ln -sf $REPODIR /var/www/dtmrepo
+chkconfig httpd on
+service httpd start
+
+if [ "$SUBSCRIBE" == "subscribe" ]; then
+    sed -i 's/^subscribed.\+/subscribed = 1/' $INSTALLDIR/etc/dtmrepo.conf
+    $INSTALLDIR/bin/dtmrepo -i
+fi
+
